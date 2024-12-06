59a4f91d8f1bf2efacfef3f39db6c47c82914546
victorolivaresat <victor.olivares@apuestatotal.com>
2024-12-05 20:54:50 -0500

    Merge

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
 const instance = axios.create({  
   baseURL: "http://192.168.21.35:5000/api/v1",
   withCredentials: true,
